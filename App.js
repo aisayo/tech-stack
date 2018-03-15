26f8ddc8f506761fc12d2185295074ea2c03affa
Aysan Isayo <aisayo@icloud.com>
2018-03-14 19:29:16 -0700

    added componentwillmount to librarylist

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import  { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './src/reducers/index'
 import { Header } from './src/components/common/Header';
-
+import LibraryList from './src/components/LibraryList'
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
     <View>
-      <Header headerText='Tech Stack' />  
+      <Header headerText='Tech Stack' /> 
+      <LibraryList /> 
     </View>
     </Provider>
   );
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import { ListView } from 'react-native';
+import { connect } from 'react-redux';
 
 
 class LibraryList extends Component {
+    componentWillMount() {
+        const ds = new ListView.DataSource({ 
+            rowHasChanged: (r1, r2) => r1 !== r2
+        });
+
+        this.DataSource = ds.cloneWithRows(this.props.libraries);
+    }
+
+    renderRow() {
+        
+    }
+
     render() {
-        return;
+        return(
+            <ListView
+                dataSource={this.dataSource}
+                renderRow={this.renderRow}
+            />
+        );
     }
 }
 
-export default LibraryList;
+const mapStateToProps = state => {
+    return { libraries: state.libraries };
+};
+
+export default connect(mapStateToProps)(LibraryList);
+//we call connect(), that returns a function with the Library List
+//conect forges a connection between react and redux part of app
